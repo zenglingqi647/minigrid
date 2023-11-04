18e82995279f05625848929450016014d78c5587
Liangyuan (Circle) Chen <circlecly@berkeley.edu>
2023-11-04 12:14:27 -0700

    add basic skill training script

---
+++ rl-starter-files/train_basic_skills.sh
@@ -0,0 +1,27 @@
+# Skill 1: Go to Object (in the same room)
+python -m scripts.train --algo a2c --env BabyAI-GoToObj-v0 --text --frames 500000
+
+# Skill 2: Open door (in the same room)
+python -m scripts.train --algo a2c --env BabyAI-OpenDoor-v0 --text --frames 500000
+
+# Skill 3: Pickup an item (in the same room)
+python -m scripts.train --algo a2c --env BabyAI-PickupDist-v0 --text --frames 500000
+
+# Skill 4: Put an item next to an item (in the same room)
+python -m scripts.train --algo a2c --env BabyAI-PutNextLocal-v0 --text --frames 500000
+
+# Skill 5: Unlock a door (in the same room)
+python -m scripts.train --algo a2c --env BabyAI-UnlockLocal-v0 --text --frames 500000
+
+# Skill 6: Find an object (in a random room)
+python -m scripts.train --algo a2c --env BabyAI-FindObjS5-v0 --text --frames 500000
+
+# Skill 7: Go to the green object (in a random room)
+python3 -m scripts.train --algo a2c --env MiniGrid-FourRooms-v0 --text --frames 500000
+
+
+# Need at least one skill that enables the agent to go to a different room.
+# Skill 6 or 7?
+
+
+
