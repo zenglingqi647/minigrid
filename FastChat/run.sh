88041c6e961620d0d01a8b4582fd91e089bc6e2d
Aufklarng <lzengaf@gmail.com>
2023-11-19 05:29:29 +0800

    from https://github.com/facebookresearch/llama.git

--- FastChat/run.sh
+++ FastChat/run.sh
@@ -1,3 +1,4 @@
-export CUDA_VISIBLE_DEVICES=0
 cd /data1/lzengaf/cs285/proj/minigrid/FastChat
+conda activate fastchat
+export CUDA_VISIBLE_DEVICES=0
 python3 -m fastchat.serve.cli --model-path lmsys/vicuna-7b-v1.5
