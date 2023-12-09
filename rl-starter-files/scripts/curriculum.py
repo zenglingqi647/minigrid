d5871a2963093dfcada03c1ccf4b3634231b0bca
zenglingqi647 <lzengaf@connect.ust.hk>
2023-12-09 10:22:26 +0800

    curriculum scheme

--- rl-starter-files/scripts/curriculum.py
+++ rl-starter-files/scripts/curriculum.py
@@ -1,27 +1,3 @@
-# goto
-'''
-GoToObj
-'''
-
-envs = [
-    "BabyAI-Open-v0", "BabyAI-OpenRedDoor-v0", "BabyAI-OpenDoor-v0", "BabyAI-OpenDoorDebug-v0",
-    "BabyAI-OpenDoorColor-v0", "BabyAI-OpenDoorLoc-v0", "BabyAI-OpenTwoDoors-v0", "BabyAI-OpenRedBlueDoors-v0",
-    "BabyAI-OpenRedBlueDoorsDebug-v0", "BabyAI-OpenDoorsOrderN2-v0", "BabyAI-OpenDoorsOrderN4-v0",
-    "BabyAI-OpenDoorsOrderN2Debug-v0", "BabyAI-OpenDoorsOrderN4Debug-v0", "BabyAI-Pickup-v0", "BabyAI-UnblockPickup-v0",
-    "BabyAI-PickupLoc-v0", "BabyAI-PickupDist-v0", "BabyAI-PickupDistDebug-v0", "BabyAI-PickupAbove-v0",
-    "BabyAI-PutNextLocal-v0", "BabyAI-PutNextLocalS5N3-v0", "BabyAI-PutNextLocalS6N4-v0", "BabyAI-PutNextS4N1-v0",
-    "BabyAI-PutNextS5N2-v0", "BabyAI-PutNextS5N1-v0", "BabyAI-PutNextS6N3-v0", "BabyAI-PutNextS7N4-v0",
-    "BabyAI-PutNextS5N2Carrying-v0", "BabyAI-PutNextS6N3Carrying-v0", "BabyAI-PutNextS7N4Carrying-v0",
-    "BabyAI-Unlock-v0", "BabyAI-UnlockLocal-v0", "BabyAI-UnlockLocalDist-v0", "BabyAI-KeyInBox-v0",
-    "BabyAI-UnlockPickup-v0", "BabyAI-UnlockPickupDist-v0", "BabyAI-BlockedUnlockPickup-v0", "BabyAI-UnlockToUnlock-v0",
-    "BabyAI-ActionObjDoor-v0", "BabyAI-FindObjS5-v0", "BabyAI-FindObjS6-v0", "BabyAI-FindObjS7-v0",
-    "BabyAI-KeyCorridor-v0", "BabyAI-KeyCorridorS3R1-v0", "BabyAI-KeyCorridorS3R2-v0", "BabyAI-KeyCorridorS3R3-v0",
-    "BabyAI-KeyCorridorS4R3-v0", "BabyAI-KeyCorridorS5R3-v0", "BabyAI-KeyCorridorS6R3-v0", "BabyAI-OneRoomS8-v0",
-    "BabyAI-OneRoomS12-v0", "BabyAI-OneRoomS16-v0", "BabyAI-OneRoomS20-v0", "BabyAI-MoveTwoAcrossS5N2-v0",
-    "BabyAI-MoveTwoAcrossS8N9-v0", "BabyAI-MiniBossLevel-v0", "BabyAI-BossLevel-v0", "BabyAI-BossLevelNoUnlock-v0"
-]
-
-
 class Curriculum:
 
     def __init__(self, env_dict, upgrade_threshold=0.6, downgrade_threshold=0.3, repeat_threshold=5):
