43b7816d2979c07b78c4b9243f35cd15deecf910
Liangyuan (Circle) Chen <circlecly@berkeley.edu>
2023-12-08 20:56:51 -0800

    change batch size to the actual amount used

---
+++ scripts/skills_v1_curriculum.sh
@@ -0,0 +1,14 @@
+# Run these if on local computer
+# python -m scripts.train --algo ppo --env BabyAI-GoToLocal-v0 --model skill-model-v1-curriculum/GoToObj --text --frames 20000000 --log-interval 10 --recurrence 20 --save-interval 15 --batch-size 1280 --frames-per-proc 40 --obs-size 11
+
+# Use these if you are on savio (more processses)
+# python -m scripts.train --algo ppo --env BabyAI-GoToLocal-v0 --model skill-model-v1-curriculum/GoToObj --text --frames 10000000 --log-interval 10 --recurrence 20 --save-interval 15 --procs 64 --batch-size 1280 --frames-per-proc 40 --obs-size 11
+
+# for name in S5N2, S6N2, S6N3, S6N4, S7N4, S7N5;
+# do
+#     python -m scripts.train --algo ppo --env BabyAI-GoToLocal$name-v0 --model skill-model-v1-curriculum/GoToObj --text --frames 1000000 --log-interval 10 --recurrence 20 --save-interval 15 --procs 64 --batch-size 1280 --frames-per-proc 40 --obs-size 11
+# done
+
+for i in $(seq 2 7)
+do python -m scripts.train --algo ppo --env BabyAI-GoToLocalS8N$i-v0 --model skill-model-v1-curriculum/GoToObj --text --frames 1000000 --log-interval 10 --recurrence 20 --save-interval 15 --procs 64 --batch-size 200 --frames-per-proc 40 --obs-size 11
+done
