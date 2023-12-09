9a111dbac4d073ace56027df408fc37e39bb3e4b
zenglingqi647 <lzengaf@connect.ust.hk>
2023-12-09 13:55:11 +0800

    skill: open

---
+++ rl-starter-files/envs/open.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+open_dict = {
+    0: {
+        "name": "OpenDoor",
+        "envs": ["BabyAI-OpenDoor-v0", "BabyAI-OpenDoorColor-v0", "BabyAI-OpenDoorLoc-v0"]
+    },
+    1: {
+        "name": "OpenTwoDoors",
+        "envs": ["BabyAI-OpenTwoDoors-v0", "BabyAI-OpenRedBlueDoors-v0"]
+    },
+    2: {
+        "name": "OpenDoorsOrderN2",
+        "envs": ["BabyAI-OpenDoorsOrderN2-v0", "BabyAI-OpenDoorsOrderN4-v0"]
+    }
+}
