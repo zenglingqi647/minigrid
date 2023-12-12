5970e8d44b2961370d8a5b2ca79b518a2cbbd06c
zenglingqi647 <lzengaf@connect.ust.hk>
2023-12-12 16:36:00 +0800

    naming conventions, reformulate

--- rl-starter-files/utils/q_planner_policy.py
+++ rl-starter-files/utils/q_planner_policy.py
@@ -12,7 +12,6 @@ from .other import device
 from torch_ac.algos.replay_buffer import ReplayBuffer
 from utils.dqn_agent import DQNAgent
 
-
 SKILL_MDL_PATH = [
     "storage/skill-model-v2/Goto-Finetune",
     "storage/skill-model-v2/Open",
@@ -21,23 +20,24 @@ SKILL_MDL_PATH = [
     # "storage/skill-model-v1-curriculum/PutNext"
 ]
 
-colors = ["red", "green", "blue", "purple", "yellow", "grey"]
-objects = ["ball", "box", "key"]
+COLORS = ["red", "green", "blue", "purple", "yellow", "grey"]
+OBJECTS = ["ball", "box", "key"]
 
-GO_TO = {i + j * 6: f"go to the {colors[i]} {objects[j]}" for j in range(len(objects)) for i in range(len(colors))}
+GO_TO = {i + j * 6: f"go to the {COLORS[i]} {OBJECTS[j]}" for j in range(len(OBJECTS)) for i in range(len(COLORS))}
 
-OPEN = {18: "open the red door", 19: "open the green door", 20: "open the blue door", 21: "open the purple door", 22: "open the yellow door", 23: "open the grey door"}
+OPEN = {18 + i: f"open the {COLORS[i]} door" for i in range(len(COLORS))}
 
-PICK_UP = {24: "pick up a red ball", 25: "pick up a green ball", 26: "pick up a blue ball", 27: "pick up a purple ball", 28: "pick up a yellow ball", 29: "pick up a grey ball", 
-30: "pick up a red box", 31: "pick up a green box", 32: "pick up a blue box", 33: "pick up a purple box", 34: "pick up a yellow box", 35: "pick up a grey box", 
-36: "pick up a red key", 37: "pick up a green key", 38: "pick up a blue key", 39: "pick up a purple key", 40: "pick up a yellow key", 41: "pick up a grey key"}
+PICK_UP = {
+    24 + i + j * 6: f"pick up a {COLORS[i]} {OBJECTS[j]}" for j in range(len(OBJECTS)) for i in range(len(COLORS))
+}
 
-UNLOCK = {42: "open the red door", 43: "open the green door", 44: "open the blue door", 45: "open the purple door", 46: "open the yellow door", 47: "open the grey door"}
+UNLOCK = {42 + i: f"open the {COLORS[i]} door" for i in range(len(COLORS))}
 
-# PUT_NEXT = {48: "put the red ball next to the red ball", 49: "put the red ball next to the green ball", 50: "put the red ball next to the blue ball", 51: "put the red ball next to the purple ball", 52: "put the red ball next to the yellow ball", 53: "put the red ball next to the grey ball", 
-# 54: "put the green ball next to the red ball", 55: "put the green ball next to the green ball", 56: "put the green ball next to the blue ball", 57: "put the green ball next to the purple ball", 58: "put the green ball next to the yellow ball", 59: "put the green ball next to the grey ball",
-
-# }
+PUT_NEXT = {
+    48 + i + j * 6 + k * 36 + l * 216: f"put the {COLORS[i]} {OBJECTS[k]} next to the {COLORS[j]} {OBJECTS[l]}"
+    for k in range(len(OBJECTS)) for l in range(len(OBJECTS))
+    for i in range(len(COLORS)) for j in range(len(COLORS)) if k != l
+}
 
 PUT_NEXT = f"put the {0} {1} next to the {2} {3}"
 
@@ -45,10 +45,21 @@ PUT_NEXT = f"put the {0} {1} next to the {2} {3}"
 
 SKILL_LIST = [GO_TO, OPEN, PICK_UP, UNLOCK, PUT_NEXT]
 
+
 class QPlannerPolicy(nn.Module, torch_ac.RecurrentACModel):
     '''ask_cooldown: how many steps to wait before asking GPT again. For synchronization.'''
-    
-    def __init__(self, obs_space, action_space, vocab, llm_variant, ask_cooldown, num_procs, use_memory=False, use_text=False, num_skills=4, llm_augmented=False):
+
+    def __init__(self,
+                 obs_space,
+                 action_space,
+                 vocab,
+                 llm_variant,
+                 ask_cooldown,
+                 num_procs,
+                 use_memory=False,
+                 use_text=False,
+                 num_skills=4,
+                 llm_augmented=False):
         super().__init__()
         # adapted from ACModel
         self.use_memory = use_memory
@@ -56,18 +67,11 @@ class QPlannerPolicy(nn.Module, torch_ac.RecurrentACModel):
 
         n = obs_space["image"][0]
         m = obs_space["image"][1]
-        self.image_embedding_size = ((n-1)//2-2)*((m-1)//2-2)*64
+        self.image_embedding_size = ((n - 1) // 2 - 2) * ((m - 1) // 2 - 2) * 64
 
         # Define image embedding
-        self.image_conv = nn.Sequential(
-            nn.Conv2d(3, 16, (2, 2)),
-            nn.ReLU(),
-            nn.MaxPool2d((2, 2)),
-            nn.Conv2d(16, 32, (2, 2)),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, (2, 2)),
-            nn.ReLU()
-        )
+        self.image_conv = nn.Sequential(nn.Conv2d(3, 16, (2, 2)), nn.ReLU(), nn.MaxPool2d((2, 2)),
+                                        nn.Conv2d(16, 32, (2, 2)), nn.ReLU(), nn.Conv2d(32, 64, (2, 2)), nn.ReLU())
         # Define text embedding
         if self.use_text:
             self.word_embedding_size = 32
@@ -79,7 +83,6 @@ class QPlannerPolicy(nn.Module, torch_ac.RecurrentACModel):
         if self.use_text:
             self.embedding_size += self.text_embedding_size
 
-
         self.obs_space = obs_space
         self.action_space = action_space
         self.num_skills = num_skills
@@ -88,19 +91,23 @@ class QPlannerPolicy(nn.Module, torch_ac.RecurrentACModel):
         self.ask_cooldown = ask_cooldown
         self.num_envs = num_procs
 
-        self.current_skills : list[int] = [0] * self.num_envs
-        self.current_goals : list[int] = [None] * self.num_envs
-        self.skill_vocabs : list[Vocabulary] = [None] * self.num_skills
-        self.vocab : Vocabulary = vocab.vocab
-        self.invert_vocab : dict = {v: k for k, v in self.vocab.items()}
-        
+        self.current_skills: list[int] = [0] * self.num_envs
+        self.current_goals: list[int] = [None] * self.num_envs
+        self.skill_vocabs: list[Vocabulary] = [None] * self.num_skills
+        self.vocab: Vocabulary = vocab.vocab
+        self.invert_vocab: dict = {v: k for k, v in self.vocab.items()}
+
         self.llm_variant = llm_variant
-        # load skill mmodel 
+        # load skill mmodel
         for i in range(num_skills):
             self.ac_models.append(self.load_model(i))
 
         self.llm_augmented = llm_augmented
-        self.dqn_agent = DQNAgent(observation_shape=self.embedding_size, num_actions=48, discount=0.99, target_update_period=1000, use_double_q=True)
+        self.dqn_agent = DQNAgent(observation_shape=self.embedding_size,
+                                  num_actions=48,
+                                  discount=0.99,
+                                  target_update_period=1000,
+                                  use_double_q=True)
 
         # self.lock = threading.Lock()
 
@@ -137,7 +144,6 @@ class QPlannerPolicy(nn.Module, torch_ac.RecurrentACModel):
             return SKILL_LIST[3][action_num]
         assert False
 
-
     def get_skills_and_goals(self, obs):
         '''
             Get the skill numbers and goals for an observation. Must ensure observation batch size is the same as the number of parallel environments
@@ -146,12 +152,12 @@ class QPlannerPolicy(nn.Module, torch_ac.RecurrentACModel):
         assert (obs.full_obs.shape[0] == self.num_envs)
         assert (obs.text.shape[0] == self.num_envs)
 
-        current_skills : list[int] = [0] * self.num_envs
-        current_goals : list[int] = [None] * self.num_envs
+        current_skills: list[int] = [0] * self.num_envs
+        current_goals: list[int] = [None] * self.num_envs
 
         for idx in range(obs.full_obs.shape[0]):
             # Extract the individual image and mission texts
-            obs_img : torch.Tensor = obs.full_obs[idx]
+            obs_img: torch.Tensor = obs.full_obs[idx]
             mission_txt = " ".join([self.invert_vocab[s.item()] for s in obs.text[idx]])
             print(f"Mission text sent is {mission_txt}")
             x = obs_img.transpose(1, 3).transpose(2, 3)
@@ -174,11 +180,10 @@ class QPlannerPolicy(nn.Module, torch_ac.RecurrentACModel):
 
             current_skills[idx] = skill_num
             current_goals[idx] = goal_tokens
-        
+
         return current_skills, current_goals
 
-
-    def forward(self, obs : DictList, memory):
+    def forward(self, obs: DictList, memory):
         # here, obs is a dictionary of batched images and batched text. The batch size is a integer multiple of the number of parallel environments.
 
         dist_logits, values, memories = [], [], []
@@ -195,18 +200,15 @@ class QPlannerPolicy(nn.Module, torch_ac.RecurrentACModel):
                 memory_one_step = memory[i + j:i + j + 1]
 
                 # Use the same image observation but change the goal
-                new_obs = DictList({"image" : obs_one_step.image, "text" : goal.unsqueeze(0)})
+                new_obs = DictList({"image": obs_one_step.image, "text": goal.unsqueeze(0)})
                 d, v, m = self.ac_models[skill_num](new_obs, memory_one_step)
 
                 dist_logits.append(d.logits)
                 values.append(v)
                 memories.append(m)
-            
+
         dist_logits = torch.cat(dist_logits)
         values = torch.cat(values)
         memories = torch.cat(memories)
 
         return Categorical(logits=dist_logits), values, memories
-
-
-    
