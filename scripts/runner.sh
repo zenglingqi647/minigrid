b94a44305b72f7b8e80b1ca6675cc5941e6a34de
C <colincai09@gmail.com>
2023-12-13 01:21:31 -0800

    update runner

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -1,6 +1,10 @@
 cd ../rl-starter-files
 
+python -m scripts.train --algo base --env BabyAI-GoToObjMaze-v0  --text --frames 15000 --log-interval 10 --recurrence 20 --save-interval 15 --batch-size 200 --procs 16 --frames-per-proc 40 --obs-size 0 --llm-planner-variant gpt --ask-every 4
+
 python -m scripts.train --algo base --env BabyAI-GoToSeq-v0 --text --frames 1000000 --log-interval 10 --recurrence 20 --save-interval 15 --batch-size 1280 --procs 64 --frames-per-proc 40 --obs-size 0 --use-dqn --llm-augmented --llm-planner-variant gpt --ask-every 4
+
+python -m scripts.train --algo base --env BabyAI-GoTo-v0 --text --frames 1000000 --log-interval 10 --recurrence 20 --save-interval 15 --batch-size 1280 --procs 64 --frames-per-proc 40 --obs-size 0 --use-dqn --llm-augmented --llm-planner-variant gpt --ask-every 4 
 
 python -m scripts.train --algo base --env BabyAI-UnblockPickup-v0 --text --frames 1000000 --log-interval 10 --recurrence 20 --save-interval 15 --batch-size 1280 --procs 64 --frames-per-proc 40 --obs-size 0 --use-dqn --llm-augmented --llm-planner-variant gpt --ask-every 4
 
@@ -8,4 +12,3 @@ python -m scripts.train --algo base --env BabyAI-Pickup-v0 --text --frames 10000
 
 python -m scripts.train --algo base --env BabyAI-GoToImpUnlock-v0 --text --frames 1000000 --log-interval 10 --recurrence 20 --save-interval 15 --batch-size 1280 --procs 64 --frames-per-proc 40 --obs-size 0 --use-dqn --llm-augmented --llm-planner-variant gpt --ask-every 4
 
-python -m scripts.train --algo base --env BabyAI-GoTo-v0 --text --frames 1000000 --log-interval 10 --recurrence 20 --save-interval 15 --batch-size 1280 --procs 64 --frames-per-proc 40 --obs-size 0 --use-dqn --llm-augmented --llm-planner-variant gpt --ask-every 4 
