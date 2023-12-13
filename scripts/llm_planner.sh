e540c44a69b31730d9db34520a3201f447b208ee
C <colincai09@gmail.com>
2023-12-12 22:49:16 -0800

    update script

--- scripts/llm_planner.sh
+++ scripts/llm_planner.sh
@@ -1,9 +1,9 @@
 cd ../rl-starter-files
 
-python -m scripts.train --algo base --env BabyAI-GoToImpUnlock-v0  --text --frames 3000000 --log-interval 10 --recurrence 20 --save-interval 15 --batch-size 1280 --procs 64 --frames-per-proc 40 --obs-size 0 --llm-planner-variant gpt --ask-every 1
+# python -m scripts.train --algo base --env BabyAI-GoToImpUnlock-v0  --text --frames 1000000 --log-interval 10 --recurrence 20 --save-interval 15 --batch-size 1280 --procs 64 --frames-per-proc 40 --obs-size 0 --llm-planner-variant gpt --ask-every 4
 
-python -m scripts.train --algo base --env BabyAI-GoToSeq-v0  --text --frames 3000000 --log-interval 10 --recurrence 20 --save-interval 15 --batch-size 1280 --procs 64 --frames-per-proc 40 --obs-size 0 --llm-planner-variant gpt --ask-every 1
+python -m scripts.train --algo base --env BabyAI-GoToSeq-v0  --text --frames 1000000 --log-interval 10 --recurrence 20 --save-interval 15 --batch-size 1280 --procs 64 --frames-per-proc 40 --obs-size 0 --llm-planner-variant gpt --ask-every 4
 
-python -m scripts.train --algo base --env BabyAI-KeyCorridorS4R3-v0  --text --frames 3000000 --log-interval 10 --recurrence 20 --save-interval 15 --batch-size 1280 --procs 64 --frames-per-proc 40 --obs-size 0 --llm-planner-variant gpt --ask-every 1
+# python -m scripts.train --algo base --env BabyAI-KeyCorridorS4R3-v0  --text --frames 1000000 --log-interval 10 --recurrence 20 --save-interval 15 --batch-size 1280 --procs 64 --frames-per-proc 40 --obs-size 0 --llm-planner-variant gpt --ask-every 4
 
-python -m scripts.train --algo base --env BabyAI-UnblockPickup-v0  --text --frames 3000000 --log-interval 10 --recurrence 20 --save-interval 15 --batch-size 1280 --procs 64 --frames-per-proc 40 --obs-size 0 --llm-planner-variant gpt --ask-every 1
+# python -m scripts.train --algo base --env BabyAI-UnblockPickup-v0  --text --frames 1000000 --log-interval 10 --recurrence 20 --save-interval 15 --batch-size 1280 --procs 64 --frames-per-proc 40 --obs-size 0 --llm-planner-variant gpt --ask-every 4
