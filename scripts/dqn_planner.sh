40102e694d5b25cce0574429fa6412a92488f8a2
Liangyuan (Circle) Chen <circlecly@berkeley.edu>
2023-12-13 01:30:37 -0800

    add sleep when rate limit

--- scripts/dqn_planner.sh
+++ scripts/dqn_planner.sh
@@ -6,6 +6,8 @@ cd ../rl-starter-files
 
 python -m scripts.train --algo base --env BabyAI-GoTo-v0 --text --frames 1000000 --log-interval 10 --recurrence 20 --save-interval 15 --batch-size 1280 --procs 64 --frames-per-proc 40 --obs-size 0 --use-dqn
 
+python -m scripts.train --algo base --env BabyAI-Pickup-v0 --text --frames 1000000 --log-interval 10 --recurrence 20 --save-interval 15 --batch-size 1280 --procs 64 --frames-per-proc 40 --obs-size 0 --use-dqn
+
 # python -m scripts.train --algo base --env BabyAI-UnblockPickup-v0  --text --frames 1000000 --log-interval 10 --recurrence 20 --save-interval 15 --batch-size 1280 --procs 64 --frames-per-proc 40 --obs-size 0 --use-dqn
 
 # python -m scripts.train --algo base --env BabyAI-KeyCorridorS4R3-v0  --text --frames 1000000 --log-interval 10 --recurrence 20 --save-interval 15 --batch-size 1280 --procs 64 --frames-per-proc 40 --obs-size 0 --use-dqn
